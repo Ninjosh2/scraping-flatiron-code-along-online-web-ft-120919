854b37a152dab6de9dbf2c6eeeb12b87c00e1f4d
Joshua Sanders <ninjosh2@gmail.com>
2020-01-15 04:01:58 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,20 @@
 class Course
   attr_accessor :title, :schedule, :descripton 
+  
+  @@all = [] 
+  
+  def initialize 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.reset_all 
+    @@all.clear 
+  end  
+  
+  
 end 
 
